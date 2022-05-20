12dbff7b97ee97197a39959c15946f99bac08365
Ivan <www.vancho@gmail.com>
2022-05-20 23:56:49 +0300

    modal forms

--- src/js/script.js
+++ src/js/script.js
@@ -38,4 +38,53 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link')
     toggleSlide('.catalog-item__back')
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow')
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function valideForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2   
+                },
+                phone: "required",
+                email:{
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Введите Имя",
+                    minlength: jQuery.validator.format("Введите {0} символов")
+                },
+                phone: " Введите телефон",
+                email: { 
+                  required: "Введите почту",
+                  email: "Введите в формате name@domain.com"
+                }
+            }
+        });
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name="phone"]').mask("+7 (999) 999-99-99");
+    
 });
